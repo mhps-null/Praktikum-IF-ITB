c8cd450f3ab6089bf4782f82f1fb8800850045b5
mhps-null <mharisputras.work@gmail.com>
2025-09-05 01:32:18 +0700

    add: Praktikum 0 Haskell ALPRO 2 Semester 3

---
+++ Kali.hs
@@ -0,0 +1,15 @@
+-- Perkalian 2 angka                            kali a b
+
+-- HEADER
+module Kali where
+
+-- DEFINSI DAN SPESIFIKASI
+kali :: Int -> Int -> Int
+    -- menerima masukan a dan b dan menghitung perkalian a*b
+    -- a dan b integer
+
+-- REALISASI
+kali a b = a * b
+
+-- APLIKASI
+-- kali 9 8
---
+++ Pangkat2.hs
@@ -0,0 +1,15 @@
+-- Pangkat 2                            pangkat2 x
+
+-- HEADER
+module Pangkat2 where
+
+-- DEFINSI DAN SPESIFIKASI
+pangkat2 :: Int -> Int
+-- menerima masukan x dan menghitung x pangkat 2 (x^2)
+-- x integer
+
+-- REALISASI
+pangkat2 x = x * x
+
+-- APLIKASI
+-- pangkat2
