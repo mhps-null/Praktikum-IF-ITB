29a344a2016aac9c6d8a5ea78c32c24d17767258
mhps-null <mharisputras.work@gmail.com>
2025-09-05 17:13:50 +0700

    add: Latihan seleksi asprak berkom

---
+++ Pengkom Python/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+buah = [4,1,5,2,5,6,36]
+buah.append(10)
+del buah[3]
+print(buah)
---
+++ Pengkom Python/tes.py
@@ -0,0 +1,41 @@
+# nama = str(input())
+# umur = int(input())
+# print(f"Halo, {nama}. Tahun depan umurmu {umur+1} tahun.")
+
+# for i in range (1,21,1):
+#     print(i)
+
+# buah = [4,1,5,2,5,6,36]
+# buah.append(10)
+# del buah[2]
+# print(buah)
+
+# def is_prime(n):
+#     if(n<2): return False
+#     for i in range(2,int(n**(1/2) + 1),1):
+#         if (n%i==0):return False
+#     return True
+# print(is_prime(0))
+# print(is_prime(-3))
+# print(is_prime(3))
+# print(is_prime(4))
+# print(is_prime(5))
+# print(is_prime(10))
+
+# angka=int(input())
+# if(angka%3==0 and angka%5==0):
+#     print("FizzBuzz")
+# elif(angka%3==0):
+#     print("Fizz")
+# elif(angka%5==0):
+#     print("Buzz")
+# else: print(angka)
+
+# mhs = {"nama": "Andi", "umur": 20, "jurusan": "Informatika"}
+# print(mhs["jurusan"])
+# mhs["umur"]+=1
+# mhs["angkatan"]=2023
+# print(mhs["umur"])
+# print(mhs["angkatan"])
+
+# print(int(25**(1/2)))
